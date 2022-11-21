4bf7e5e61bb8bb3443e591f9d615efd21f89b1ab
Vissarion Moutafis <44686370+VissaMoutafis@users.noreply.github.com>
2022-11-21 12:14:29 +0000

    packaging

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ setup(
     name='fastapi-cli',
     version='0.0.1',
     author='VissaMoutafis',
+    author_email='vissarionm2000@gmail.com'
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
         'fastapi[all]',
-        'gitPython'
+        'gitPython',
+        'git+https://github.com/VissaMoutafis/fastapi-cli'
     ],
     entry_points={
         'console_scripts': [
